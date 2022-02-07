2e7eba1ddc4977217f3883e78cacfe8f28aab942
Daniel Klotz <danielfelipe.klotz@logmein.com>
2022-02-07 19:39:48 +0100

    feat: add all events page

--- pages/events/index.tsx
+++ pages/events/index.tsx
@@ -1,7 +1,12 @@
+import EventList from "../../components/events/event-list";
+import { getAllEvents } from "../../dummy-data";
+
 const AllEventsPage = () => {
+  const events = getAllEvents();
+
   return (
     <div>
-      <h1>All events Page</h1>
+      <EventList events={events} />
     </div>
   );
 };
